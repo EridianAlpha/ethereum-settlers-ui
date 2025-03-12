8bf1c472184a87ce7e7a961b655ffbcfed2cfa1d
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-12 22:50:00 +0000

    Add Google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import customTheme from "../styles/customTheme"
 
 import { ChakraProvider, ColorModeScript, useColorModeValue } from "@chakra-ui/react"
 
+import GoogleAnalytics from "@/components/GoogleAnalytics"
+
 import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
@@ -54,6 +56,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <Head>
                     <title>Ethereum Settlers</title>
                 </Head>
+                <GoogleAnalytics />
                 <Component {...pageProps} />
                 <Analytics />
             </div>
